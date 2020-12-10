0f4f5df077af0032b84cc9c5105661861f2c76e2
acutemihawk <57567300+acutemihawk@users.noreply.github.com>
2020-12-10 18:27:23 +0100

    classes added
    classes Offer and Advertisment added

---
+++ src/Advertisment.java
@@ -0,0 +1,15 @@
+
+public class Advertisment
+{
+	private	int idAdvertisment;
+	private int idOwner;
+	private String type;
+	private String category;
+	private float price;
+	private String description;
+	private ArrayList<Offer> ListMyOffer;
+	
+	
+	
+	
+}

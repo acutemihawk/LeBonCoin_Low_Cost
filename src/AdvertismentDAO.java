6f879824d507d18e52b9a6f0ae93cd5c87886b05
acutemihawk <57567300+acutemihawk@users.noreply.github.com>
2020-12-10 20:13:35 +0100

    Update AdvertismentDAO.java

--- src/AdvertismentDAO.java
+++ src/AdvertismentDAO.java
@@ -1,5 +1,29 @@
 
 public class AdvertismentDAO
 {
+	public boolean insertAd(Advertisment Ad)
+	{
+		//à faire
+		return true;
+	}
+	
+	public boolean deleteAd(Advertisment Ad)
+	{
+		//à faire
+		return true;
+	}
+	
+	public boolean updateAd(Advertisment Ad)
+	{
+		//à faire
+		return true;
+	}
+	
+	public boolean getLastID()
+	{
+		//à faire
+		return true;
+	}
+	
 	
 }

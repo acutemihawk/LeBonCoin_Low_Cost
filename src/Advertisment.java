0735e56409cfe76f6cde8eb95d3f7aa56b56e2d3
acutemihawk <57567300+acutemihawk@users.noreply.github.com>
2020-12-10 18:54:11 +0100

    Update Advertisment.java

--- src/Advertisment.java
+++ src/Advertisment.java
@@ -11,6 +11,77 @@ public class Advertisment
 	private ArrayList<Offer> listMyOffer;
 	
 	
+	public int getIdAdvertisment()
+	{
+		return idAdvertisment;
+	}
+	
+	public void setIdAdvertisment(int idAdvertisment)
+	{
+		this.idAdvertisment = idAdvertisment;
+	}
+	
+	public int getIdOwner()
+	{
+		return idOwner;
+	}
+	
+	public void setIdOwner(int idOwner)
+	{
+		this.idOwner = idOwner;
+	}
+	
+	public String getType()
+	{
+		return type;
+	}
+	
+	public void setType(String type)
+	{
+		this.type = type;
+	}
+	
+	public String getCategory()
+	{
+		return category;
+	}
+	
+	public void setCategory(String category)
+	{
+		this.category = category;
+	}
+	
+	public float getPrice()
+	{
+		return price;
+	}
+	
+	public void setPrice(float price)
+	{
+		this.price = price;
+	}
+	
+	public String getDescription()
+	{
+		return description;
+	}
+	
+	public void setDescription(String description)
+	{
+		this.description = description;
+	}
+	
+	public ArrayList<Offer> getListMyOffer()
+	{
+		return listMyOffer;
+	}
+	
+	public void setListMyOffer(ArrayList<Offer> listMyOffer)
+	{
+		this.listMyOffer = listMyOffer;
+	}
+	
+	
 	
 	
 }

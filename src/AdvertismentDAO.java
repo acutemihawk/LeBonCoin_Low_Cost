6eebfa871d7cd427b65484b8dbac98976f200766
acutemihawk <57567300+acutemihawk@users.noreply.github.com>
2020-12-11 13:00:10 +0100

    Update AdvertismentDAO.java

--- src/AdvertismentDAO.java
+++ src/AdvertismentDAO.java
@@ -17,7 +17,7 @@ public class AdvertismentDAO
 								+ ad.getPrice() + "', '"
 								+ ad.getDescription() + "', '"
 								+ ad.getCategory() + "', '"
-								+ ad.getIdOwner() + "')";
+								+ ad.getIdOwner() + "');";
 			Statement myStatement = myConnection.createStatement();
 			myStatement.executeUpdate(sqlCommand);
 			
@@ -36,8 +36,26 @@ public class AdvertismentDAO
 	
 	public boolean deleteAd(Advertisment ad)
 	{
-		//à faire
-		return true;
+		Database myDB = new Database();
+		Connection myConnection = myDB.connect();
+		
+		try
+		{
+			String sqlCommand = "DELETE FROM advertisment WHERE IdAdvertisment=" + ad.getIdAdvertisment() + "');";
+			Statement myStatement = myConnection.createStatement();
+			myStatement.executeUpdate(sqlCommand);
+			
+			myStatement.close();
+			myDB.disconnect();
+			
+			return true;
+		}
+		catch (SQLException e)
+		{
+			System.out.println(e.getMessage());
+			myDB.disconnect();
+			return false;
+		}
 	}
 	
 	public boolean updateAd(Advertisment ad)

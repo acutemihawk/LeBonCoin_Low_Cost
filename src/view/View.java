9c4fe1f1a6ec491ac76fdbc6ee0cef9bf902cf07
acutemihawk <57567300+acutemihawk@users.noreply.github.com>
2020-12-14 14:53:20 +0100

    Update View.java

--- src/view/View.java
+++ src/view/View.java
@@ -1,10 +1,43 @@
 package view;
 
+import java.util.Scanner;
+
+import controller.*;
+import model.*;
+
 public class View
 {
+	private boolean isUserConnected = false;
+	private Scanner myScanner = new Scanner(System.in);
 	
-	public void MainMenu()
+	public void mainMenu()
 	{
 		
 	}
+	
+	public void browse()
+	{
+		
+	}
+	
+	public void createAdvertisment()
+	{
+		
+	}
+	
+	public void createOffer()
+	{
+		
+	}
+	
+	public void displayCategories()
+	{
+		
+	}
+	
+	public void displayAdvertisments()
+	{
+		
+	}
+	
 }

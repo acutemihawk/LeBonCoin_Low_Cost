b6e39bef66a3fbacb4cf9605891c390bf9a5c05a
acutemihawk <57567300+acutemihawk@users.noreply.github.com>
2020-12-14 23:31:27 +0100

    Update View.java

--- src/view/View.java
+++ src/view/View.java
@@ -13,9 +13,28 @@ public class View
 	
 	public void mainMenu()
 	{
+		System.out.println("Choose one option from below to navigate :");
 		System.out.println("1 - Sign in");
 		System.out.println("2 - Continue without signing in");
-		
+		System.out.println("3 - Create account");
+		int x = 0;
+		if(x == 1)
+		{
+			System.out.println("met tes id");
+			String username = "kaaris";
+			
+			System.out.println("mdp");
+			String password = "password";
+			
+			if(mainController.userConnect(username, password))
+			{
+				
+			}
+			else
+			{
+				
+			}
+		}
 		
 		
 	}

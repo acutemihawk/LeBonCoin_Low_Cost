6178ef7c870d9ddaabf2d41fc7d2c586968e9b70
acutemihawk <57567300+acutemihawk@users.noreply.github.com>
2020-12-16 13:13:59 +0100

    Update View.java

--- src/view/View.java
+++ src/view/View.java
@@ -1,6 +1,7 @@
 package view;
 
 import java.sql.Connection;
+import java.sql.PreparedStatement;
 import java.sql.ResultSet;
 import java.sql.SQLException;
 import java.sql.Statement;
@@ -27,10 +28,10 @@ public class View
 		System.out.println("3 - Create account");
 		System.out.println("--------------------------------------------------");
 		
-		option_number = myScanner.nextInt();
-		
 		try
 		{
+			option_number = myScanner.nextInt();
+
 			if(option_number == 1)
 			{
 				System.out.println("username:");
@@ -48,7 +49,7 @@ public class View
 				}
 				else
 				{
-					System.out.println("Could not connect, please verify your username or password.");
+					System.out.println("Error: Could not connect, please verify your username or password.");
 					System.out.println("You can also create your account.");
 					
 					mainMenu();
@@ -90,8 +91,6 @@ public class View
 		
 		try
 		{
-			
-			
 			String sqlCommand = "SELECT DISTINCT catégorie FROM advertisment";
 			Statement myStatement = myConnection.createStatement();
 			ResultSet rs  = myStatement.executeQuery(sqlCommand);
@@ -127,10 +126,10 @@ public class View
 		}
 		System.out.println("--------------------------------------------------");
 		
-		option_number = myScanner.nextInt();
-		
 		try
 		{
+			option_number = myScanner.nextInt();
+			
 			if(option_number == 1)
 			{
 				search();
@@ -142,13 +141,12 @@ public class View
 				else
 					mainMenu();
 			}
-			else if(option_number == 3)
+			else if(option_number > 2)
 			{
 				//check si la valeur entrée est supérieure à 2 
 				//et inférieure au nb de catégories
 				//ensuite on appelle la méthode display qui display les advertisments par catégories
 			}
-			
 		}
 		catch(NumberFormatException myException)
 		{
@@ -163,29 +161,126 @@ public class View
 	//fonction creation d'un compte
 	public void createAccount()
 	{
+		String username;
+		String password;
+		String mail;
 		
+		System.out.println("username:");
+		given_Str = myScanner.nextLine();
+		username = given_Str;
+		
+		System.out.println("password:");
+		given_Str = myScanner.nextLine();
+		password = given_Str;
+
+		System.out.println("email address:");
+		given_Str = myScanner.nextLine();
+		mail = given_Str;
+		
+		mainController.createAccount(username, password, mail);
+		
+		if(mainController.userConnect(username, password))
+		{
+			mainController.getMyUser().setConnected(true);
+			connectedUser();
+		}
+		else
+		{
+			System.out.println("Error: Could not connect, please verify your username or password.");
+			mainMenu();
+		}
 	}
 	
 	//fonction search avec paramètres
 	public void search()
 	{
-		System.out.println("Category:");
-		given_Str = myScanner.nextLine();
-		String category = given_Str;
-		
-		System.out.println("Localisation:");
-		given_Str = myScanner.nextLine();
-		String localisation = given_Str;
-		
-		System.out.println("Minimum price:");
-		given_Str = myScanner.nextLine();
-		String minPrice = given_Str;
-		
-		System.out.println("Maximum price:");
-		given_Str = myScanner.nextLine();
-		String maxPrice = given_Str;
+		String category = null;
+		String localisation = null;
+		String minPrice = null;
+		String maxPrice = null;
+		//*******************************************penser à changer prix en long osef des float***
+
+		try
+		{
+			option_number = myScanner.nextInt();
+			
+			if(option_number == 1)
+			{
+				System.out.println("Category:");
+				given_Str = myScanner.nextLine();
+				category = given_Str;
+			}
+			else if(option_number == 2)
+			{
+				System.out.println("Localisation:");
+				given_Str = myScanner.nextLine();
+				localisation = given_Str;
+			}
+			else if(option_number == 3)
+			{
+				System.out.println("Minimum price:");
+				given_Str = myScanner.nextLine();
+				minPrice = given_Str;
+			}
+			else if(option_number == 4)
+			{
+				System.out.println("Maximum price:");
+				given_Str = myScanner.nextLine();
+				maxPrice = given_Str;
+			}
+			else if(option_number == 5)
+			{
+				browse();
+			}
+		}
+		catch(NumberFormatException myException)
+		{
+			System.out.println("L'argument de la commande Push doit etre entier !");
+		}
+		catch(InputMismatchException myException) //à voir si on doit le garder
+		{
+			System.out.println("L'argument de doit etre un identifiant (nombre entier)");
+		}
 		
 		String[] arrayToSearch = {category, localisation, minPrice, maxPrice};
+		
+		Database myDB = new Database();
+		Connection myConnection = myDB.connect();
+		
+		try
+		{
+			String sqlCommand = "SELECT  FROM advertisment";
+			
+			for(int commandLoop = 0; commandLoop < arrayToSearch.length; commandLoop++)
+			{
+				if(arrayToSearch[commandLoop] != null)
+				{
+					switch(commandLoop)
+					{
+						case 0:
+							sqlCommand += " WHERE category=?";
+						case 1:
+							
+					}
+				}
+			}
+			
+			Statement myStatement = myConnection.createStatement();
+			ResultSet rs  = myStatement.executeQuery(sqlCommand);
+			
+			if(rs.next())
+			{
+				
+			}
+			
+			myStatement.close();
+			myDB.disconnect();
+		}
+		catch (SQLException e) 
+		{
+			System.out.println(e.getMessage());
+			myDB.disconnect();
+		}
 		
 	}
 	
